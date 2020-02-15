baf5ee8bc513799433b35d1c77c7890f693b3294
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-02-15 10:06:17 -0600

    added material design icons and matrial layout grid

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileFormComponent } from './components/profile-form/profile-form.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProfileFormComponent],
   imports: [
     CommonModule
   ]
