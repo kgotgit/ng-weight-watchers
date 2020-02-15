1dd0c6eec893fdd906ef3166b1d42b460c99d4b2
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-02-14 23:18:20 -0600

    initialize app tool bar section

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../shared/material/material.module';
+import { TopToolbarComponent } from './components/top-toolbar/top-toolbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TopToolbarComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialModule
+  ],
+  exports:[TopToolbarComponent]
 })
 export class CoreModule { }
