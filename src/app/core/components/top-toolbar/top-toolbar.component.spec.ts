c1922c0c5b5ee77a7ab3590061a12ba3cb1302df
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-02-15 16:56:54 -0600

    top tool bar test scenario

--- src/app/core/components/top-toolbar/top-toolbar.component.spec.ts
+++ src/app/core/components/top-toolbar/top-toolbar.component.spec.ts
@@ -1,18 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TopToolbarComponent } from './top-toolbar.component';
+import { DebugElement } from '@angular/core';
+import { CoreModule } from '../../core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('TopToolbarComponent', () => {
   let component: TopToolbarComponent;
   let fixture: ComponentFixture<TopToolbarComponent>;
+  let el:DebugElement;
 
+ 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TopToolbarComponent ]
+      imports: [CoreModule,BrowserAnimationsModule]
     })
-    .compileComponents();
-  }));
+      .compileComponents()
+      .then(() => {
 
+        fixture = TestBed.createComponent(TopToolbarComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+
+      });
+  }));
   beforeEach(() => {
     fixture = TestBed.createComponent(TopToolbarComponent);
     component = fixture.componentInstance;
