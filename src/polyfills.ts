8e7863d6b5c337fbd136ced429b7199249f2cc53
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-02-14 22:39:35 -0600

    angular material and angular elements depedencies are added

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,11 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'document-register-element';
+
+
+// Used for browsers with partially native support of Custom Elements
+import '@webcomponents/custom-elements/src/native-shim';
+
+// Used for browsers without a native support of Custom Elements
+import '@webcomponents/custom-elements/custom-elements.min';
